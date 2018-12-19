64a478a4f43dbb8f4c16d597b3a8be25d134e1a3
Gloria Faley <45109944+gloriafaley@users.noreply.github.com>
2018-12-19 12:38:41 -0500

    Create publishPubSub.py

---
+++ publishPubSub.py
@@ -0,0 +1,10 @@
+import os
+from google.cloud import pubsub_v1
+
+publisher = pubsub_v1.PublisherClient()
+topic_name = 'projects/{project_id}/topics/{topic}'.format(
+    project_id=os.getenv('GOOGLE_CLOUD_PROJECT'),
+    topic='MY_TOPIC_NAME',  # Set this to something appropriate.
+)
+publisher.create_topic(topic_name)
+publisher.publish(topic_name, b'My first message!', spam='eggs')
